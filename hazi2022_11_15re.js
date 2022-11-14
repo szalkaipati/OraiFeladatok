eb0bef0195c1e3e94b24c149a6b5576126d4aea2
szalkaipati <113940144+szalkaipati@users.noreply.github.com>
2022-11-14 22:29:01 +0100

    Add files via upload

---
+++ hazi2022_11_15re.js
@@ -0,0 +1,15 @@
+var list=[5,3,9,8,10]
+console.log("Tömb ekemeunek a száma:",lists.length)
+
+let hazi =    [
+        ["5", "3", "9"],
+        ["10", "11", "20"],
+        ["szöveg", "nemszam", "szoveg"]
+]
+for(let i = 0; i < hazi.length; i++){
+
+    for(let j = 0; j < hazi[i].length; j++){
+
+        console.log(hazi[i][j]);
+    }
+}
