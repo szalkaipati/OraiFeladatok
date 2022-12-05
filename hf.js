45358942b7bf67231991c7e94c7f2cef1b06947d
szalkaipati <113940144+szalkaipati@users.noreply.github.com>
2022-12-05 22:24:20 +0100

    Add files via upload

---
+++ hf.js
@@ -0,0 +1,19 @@
+/*
+hf.:
+ 1.stringben van egy mondat es visszafele kiirasa. (js)
+2. van egy tomb amiben szinek vannak jelennitsd meg egy listaban a szineket es az adott listaelem rakattintasaval a hattert valtoztasd meg olyan szinure. (js)
+
+3.objektum egy tömbben. Tartalmazza: név, cím, kedvenc, amelyik névre kattintunk annak az adatait írja ki <p>-ben
+*/
+
+//1.Feladat:
+var szov="Decemberben van karácsony."
+var viszov="";
+for(var i=szov.length-1; i>=0; i--){
+    return szov[i]
+}
+var eredm=document.getElementById("eredmeny");
+eredm.innerHTML=viszov
+
+//2.f-nemtudom
+//3.f-nemtudom
